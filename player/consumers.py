67049ba30d0a78917b2d6454b7ad039e07d53be0
fqds <121273hvv@gmail.com>
2022-10-24 20:24:10 +0300

    item modifiers give + to stats, modifiers and stats updates automaticaly

--- player/consumers.py
+++ player/consumers.py
@@ -36,9 +36,12 @@ class PlayerConsumer(AsyncJsonWebsocketConsumer):
             if command == "create_item_modifier":
                 await create_item_modifier(content.get("item_id"), content.get("lobby_name"), self.scope["user"].pk)
             if command == "update_item_modifier":
-                await update_item_modifier(content.get("lobby_name"), self.scope["user"].pk, content.get("item_id"), content.get("modifier_id"), content.get("modifier_value"))
+                data = await update_item_modifier(content.get("lobby_name"), self.scope["user"].pk, content.get("item_id"), content.get("modifier_id"), content.get("modifier_value"))
+                if data[0]: await parameter_update(data[0][0], data[0][1], content.get("lobby_name"), self.scope["user"].pk )
+                if data[1]: await parameter_update(data[1][0], data[1][1], content.get("lobby_name"), self.scope["user"].pk )
             if command == "delete_item_modifier":
-                await delete_item_modifier(content.get("lobby_name"), self.scope["user"].pk, content.get("item_id"), content.get("modifier_id"))
+                data = await delete_item_modifier(content.get("lobby_name"), self.scope["user"].pk, content.get("item_id"), content.get("modifier_id"))
+                if data[0]: await parameter_update(data[0][0], data[0][1], content.get("lobby_name"), self.scope["user"].pk )
         except Exception as e:
             print(e)
 
@@ -68,7 +71,7 @@ def parameter_update(parameter_id, parameter_value, lobby_name, user_id):
                     data.append([i.parameter_id, parameter.parameter_value])
                 except:
                     pass
-        
+    return [parameter.id, parameter.parameter_value]
 @database_sync_to_async
 def create_item(lobby_name, user_id):
     lobby = Lobby.objects.get(lobby_name=lobby_name)
@@ -125,19 +128,47 @@ def update_item_modifier(lobby_name, user_id, item_id, modifier_id, modifier_val
     player = lobby.players.get(player_id=user_id)
     item = player.items.get(item_id=item_id)
     modifier = item.modifiers.get(modifier_id=modifier_id)
+    parameter_update_before = return_modifier_parameter_update(modifier, lobby, player, isNew=False)
+    parameter_update_after = return_modifier_parameter_update(modifier, lobby, player, isNew=True)
+    try: 
+        if modifier_value[0] == "+":
+            modifier_value_new = modifier_value.split()
+            parameter_id = lobby.lobby_parameters.get(parameter_stat = modifier_value_new[1]).parameter_id
+            parameter = player.parameters.get(parameter_id = parameter_id)
+            parameter.parameter_value = int(parameter.parameter_value) + int(modifier_value_new[2])
+            parameter.save()
+            parameter_update_after= [parameter_id, parameter.parameter_value]
+    except: pass
     modifier.modifier_value = modifier_value
     modifier.save()
     update = UpdateItemModifier(lobby_identifier=lobby, player_id=user_id, item_id=item_id, modifier_id=modifier_id, modifier_value=modifier_value)
     update.save()
+    return [parameter_update_before, parameter_update_after]
 
 @database_sync_to_async
 def delete_item_modifier(lobby_name, user_id, item_id, modifier_id):
     lobby = Lobby.objects.get(lobby_name=lobby_name)
     player = lobby.players.get(player_id=user_id)
     item = player.items.get(item_id=item_id)
+    modifier = item.modifiers.get(modifier_id=modifier_id)
+    parameter_update_before = return_modifier_parameter_update(modifier, lobby, player, isNew=False)
     item.modifiers.get(modifier_id=modifier_id).delete()
     for i in item.modifiers.filter(modifier_id__gt=modifier_id):
         i.modifier_id -= 1
         i.save()
     update = UpdateDeleteItemModifier(lobby_identifier=lobby, player_id=user_id, item_id=item_id, modifier_id=modifier_id)
     update.save()
+    return [parameter_update_before]
+
+def return_modifier_parameter_update(modifier, lobby, player, isNew):
+    try:
+        if modifier.modifier_value[0] == "+":
+            if isNew: modifier_value = modifier_value.split() 
+            else: modifier_value = modifier.modifier_value.split()
+            parameter_id = lobby.lobby_parameters.get(parameter_stat = modifier_value[1]).parameter_id
+            parameter = player.parameters.get(parameter_id = parameter_id)
+            if isNew: parameter.parameter_value = int(parameter.parameter_value) + int(modifier_value[2])
+            else: parameter.parameter_value = int(parameter.parameter_value) - int(modifier_value[2])
+            parameter.save()
+            return [parameter_id, parameter.parameter_value]
+    except: return False
